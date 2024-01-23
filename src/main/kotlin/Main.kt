fabf07d103f5e2555d7479b274ed2d2336b84f85
Saranya Sankaranarayanan <2827393s@student.gla.ac.uk>
2024-01-23 12:23:15 +0000

    Added secondary constructors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -74,18 +74,21 @@ fun main() {
 }
 
 fun buildAquarium() {
-    val aquarium=Aquarium()
-    aquarium.printSize() // print class
-    // default height and length
-    val aquarium2 = Aquarium(width = 25)
-    aquarium2.printSize()
-    // default width
-    val aquarium3 = Aquarium(height = 35, length = 110)
-    aquarium3.printSize()
-    // everything custom
-    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
-    aquarium4.printSize()
+//    val aquarium=Aquarium()
+//    aquarium.printSize() // print class
+//    // default height and length
+//    val aquarium2 = Aquarium(width = 25)
+//    aquarium2.printSize()
+//    // default width
+//    val aquarium3 = Aquarium(height = 35, length = 110)
+//    aquarium3.printSize()
+//    // everything custom
+//    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+//    aquarium4.printSize()
 
+
+    val aquarium5=Aquarium(numberOfFish = 29)
+    aquarium5.printSize()
 }
 
 fun swim(speed: String = "fast") {
--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -21,6 +21,14 @@ class Aquarium(var width:Int=20,var height:Int=40,var length:Int=100) { // <-- C
         // 1 liter = 1000 cm^3
         println("Volume: ${width * length * height / 1000} l")
     }
+
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        // calculate the height needed
+        this.height = (tank / (this.length * this.width)).toInt()
+    }
+
     fun printSize() { // <--- First version
         println("Width: $width cm " +
                 "Length: $length cm " +
